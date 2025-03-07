0ee4c8024388dd8b7a85767d8067751b34c4cafd
ESSONO-NZOGHE Eli-joël-emile <nzoghessono@gmail.com>
2025-03-07 22:37:34 +0100

    Widget filtre de Produit

--- app.py
+++ app.py
@@ -18,3 +18,9 @@ y_axis = st.selectbox("Choisissez la colonne pour l'axe Y", data.columns)
 title = st.text_input("Titre du graphique", "Titre...")
 chart = alt.Chart(data).mark_bar().encode(x=alt.X(x_axis, title=x_axis),y=alt.Y(y_axis, title=y_axis)).properties(title=title)
 st.altair_chart(chart, use_container_width=True)
+
+#Widget filtre de Produit
+produits = data['Produit'].unique()
+choix_produit = st.selectbox("Choisissez un produit :", produits)
+df_filtre = data[data['Produit'] == choix_produit]
+st.write(df_filtre)
