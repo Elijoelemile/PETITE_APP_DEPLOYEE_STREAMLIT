d53f45feabd0fbc0981d35e5efb3fe433ff4eebb
ESSONO-NZOGHE Eli-joël-emile <nzoghessono@gmail.com>
2025-03-07 22:33:24 +0100

    Structure de l'application

--- app.py
+++ app.py
@@ -6,3 +6,9 @@ import altair as alt
 #Création de DataFrame
 data = pd.read_csv('ventes.csv', sep=';')
 data.head()
+
+#Structure de l'application
+st.title("Analyse des Ventes")
+st.subheader("Un aperçu rapide des données")
+st.write(data.head())
+
