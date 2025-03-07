a6eca7c5a735f546007736f82c75d5635cf279e9
ESSONO-NZOGHE Eli-joël-emile <nzoghessono@gmail.com>
2025-03-07 22:35:23 +0100

    Visualisation graphique

--- app.py
+++ app.py
@@ -12,3 +12,9 @@ st.title("Analyse des Ventes")
 st.subheader("Un aperçu rapide des données")
 st.write(data.head())
 
+#Visualisation graphique
+x_axis = st.selectbox("Choisissez la colonne pour l'axe X", data.columns)
+y_axis = st.selectbox("Choisissez la colonne pour l'axe Y", data.columns)
+title = st.text_input("Titre du graphique", "Titre...")
+chart = alt.Chart(data).mark_bar().encode(x=alt.X(x_axis, title=x_axis),y=alt.Y(y_axis, title=y_axis)).properties(title=title)
+st.altair_chart(chart, use_container_width=True)
